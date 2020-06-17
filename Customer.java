cba0259018ece89e5cacf73d1b93d00f24ea559c
pabitra <66096898+pabitrabst@users.noreply.github.com>
2020-06-17 17:33:22 -0500

    Add files via upload

--- Customer.java
+++ Customer.java
@@ -1,6 +1,7 @@
 package com.bank;
 
 public class Customer {
+	
 	private String fname;
 	private String lname;
 	private String mname;
@@ -8,52 +9,45 @@ public class Customer {
 	private Account account1;
 	private Account account2;
 	
-	public Account getCheckingAccount() {
-		return account1;
-	}
-
-	public void setCheckingAccount(Account account1) {
-		this.account1 = account1;
-	}
-
-	public Account getSavingAccount() {
-		return account2;
-	}
-
-	public void setSavingAccount(Account account2) {
-		this.account2 = account2;
-	}
-
+	
 	public String getFname() {
 		return fname;
 	}
-
 	public void setFname(String fname) {
 		this.fname = fname;
 	}
-
 	public String getLname() {
 		return lname;
 	}
-
 	public void setLname(String lname) {
 		this.lname = lname;
 	}
-
 	public String getMname() {
 		return mname;
 	}
-
 	public void setMname(String mname) {
 		this.mname = mname;
 	}
-
 	public long getPhoneNumber() {
 		return phoneNumber;
 	}
-
 	public void setPhoneNumber(long phoneNumber) {
 		this.phoneNumber = phoneNumber;
 	}
+	public Account getAccount1() {
+		return account1;
+	}
+	public void setAccount1(Account account1) {
+		this.account1 = account1;
+	}
+	public Account getAccount2() {
+		return account2;
+	}
+	public void setAccount2(Account account2) {
+		this.account2 = account2;
+	}
+	
+	
+	
 
 }
