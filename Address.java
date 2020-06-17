574c724970d761b04a0f4495a9450345cc50aa6a
pabitra <66096898+pabitrabst@users.noreply.github.com>
2020-06-17 17:32:42 -0500

    Add files via upload

---
+++ Address.java
@@ -0,0 +1,37 @@
+package com.bank;
+
+public class Address {
+	
+	private String strtName;
+	private String city;
+	private String  state;
+	private int zip;
+	
+	
+	public String getStrtName() {
+		return strtName;
+	}
+	public void setStrtName(String strtName) {
+		this.strtName = strtName;
+	}
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+	public String getState() {
+		return state;
+	}
+	public void setState(String state) {
+		this.state = state;
+	}
+	public int getZip() {
+		return zip;
+	}
+	public void setZip(int zip) {
+		this.zip = zip;
+	}
+	
+
+}
