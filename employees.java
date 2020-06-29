e8925b1bb41130be02b923e12f3f025f0e6cf93b
shubhang-gautam-22 <66652669+shubhang-gautam-22@users.noreply.github.com>
2020-06-29 18:19:12 -0500

    Add files via upload

---
+++ employees.java
@@ -0,0 +1,53 @@
+
+class employees {
+	private String Fname;
+    private String Mname;
+    private String Lname;
+    private long ph;
+    private int id;
+    private int Salary;
+    private String dept;
+	public String getFname() {
+		return Fname;
+	}
+	public void setFname(String fname) {
+		Fname = fname;
+	}
+	public String getMnane() {
+		return Mname;
+	}
+	public void setMnane(String mnane) {
+		Mname = mname;
+	}
+	public String getLname() {
+		return Lname;
+	}
+	public void setLname(String lname) {
+		Lname = lname;
+	}
+	public long getPh() {
+		return ph;
+	}
+	public void setPh(long ph) {
+		this.ph = ph;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public int getSalary() {
+		return Salary;
+	}
+	public void setSalary(int salary) {
+		Salary = salary;
+	}
+	public String getDept() {
+		return dept;
+	}
+	public void setDept(String dept) {
+		this.dept = dept;
+	}
+    
+}
