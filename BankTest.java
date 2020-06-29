f6e841f112de1e7cb8d7231eb1a714a5a53e24e1
shubhang-gautam-22 <66652669+shubhang-gautam-22@users.noreply.github.com>
2020-06-29 18:18:18 -0500

    Add files via upload

--- BankTest.java
+++ BankTest.java
@@ -1,45 +1,65 @@
-package com.bank;
 
 public class BankTest {
 	public static void main(String[] args) {
-	
+			Account a1 = new Account();
+			a1.setAccountNo(525666200);
+			a1.setBalance(56345);
+			System.out.println(a1.getBalance());
+			System.out.println(a1.getAccountNo());
+			
+			Account a2 = new Account ();
+			a2.setAccountNo(525666200);
+			a2.setBalance(56623);
+			System.out.println(a2.getBalance());
+			System.out.println(a2.getAccountNo());
+			
+			
+			 Custom customer1 = new Custom();
+		        customer1.setFname("Ramesh");
+		        customer1.setLname("Tiwari");
+		        customer1.setPhoneNumber(2145006158);
+		        customer1.setCheckingAccount(a1);
+		        customer1.setSavingAccount(a2);
+		        System.out.println(customer1.getFname());
+		        System.out.println(customer1.getLname());
+		        System.out.println(customer1.getPhoneNumber());
+		        System.out.println(customer1.getCheckingAccount());
+		        System.out.println(customer1.getSavingAcoount());
+		        
 
-		Account a1 = new Account();
-		Account a2 = new Account();
+		        Custom customer2 = new Customer();
+		        customer2.setFname("Richa");
+		        customer2.setLname("Bashyal");
+		        customer2.setPhoneNumber(51245158);
+		        customer2.setCheckingAccount(a1);
+		        customer2.setSavingAccount(a2);
+		        System.out.println(customer1.getFname());
+		        System.out.println(customer1.getLname());
+		        System.out.println(customer1.getPhoneNumber());
+		        System.out.println(customer1.getSavingAcoount());
+		        
+		        employees e1 = new employees();
+		        e1.setFname("Bishal");
+		        e1.setMname("Adhikari");
+		        e1.setLname ("Lammichane");
+		        e1.setPh(4695866566);
+		        e1.setId(2565230);
+		        System.out.println(e1.getFname());
+		        System.out.println(e1.getLname());
+		        System.out.println(e1.getMname());
+		        System.out.println(e1.getLname());
+		        System.out.println(e1.getPh());
+		        System.out.println(e1.getId());
+		        
+		        
+		        
+		        
 
-		a1.setAccountNo(1234567);
-		a1.setBalance(1000);
-		a1.setType("checking");
-
-		a2.setAccountNo(9998898);
-		a2.setBalance(15000);
-		a2.setType("Saving");
-
-		Customer customer1 = new Customer();
-
-		customer1.setFname("Roshan");
-		customer1.setLname("Thapa");
-		customer1.setPhoneNumber(2145006158);
-		customer1.setCheckingAccount(a1);
-		customer1.setSavingAccount(a2);
-
-		System.out.println(customer1.getSavingAccount().getBalance());
-
-		boolean successful = customer1.getSavingAccount().withdraw(5000);
-		print(successful);
-		boolean success = customer1.getSavingAccount().withdraw(35000);
-		print(success);
-		boolean success1 = customer1.getSavingAccount().withdraw(10000);
-		print(success1);
-
-		System.out.println(customer1.getSavingAccount().getBalance());
+		        
+		        boolean successful = customer1.getSavingAccount().withdraw(15050);
+		        System.out.println(successful);
 	}
-
-	public static void print(boolean value) {
-		if (value)
-			System.out.println("Here is your cash");
-		else
-			System.out.println("Insufficient Balance");
-
-	}
+		
+		
+		}
 }
