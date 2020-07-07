14bdf537e63f0e5f4ad2fa0bfec6c82f478d13bc
rabinkhadka2000 <66097376+rabinkhadka2000@users.noreply.github.com>
2020-07-07 18:10:47 -0400

    Add files via upload

--- Bank.java
+++ Bank.java
@@ -1,7 +1,16 @@
 public class Bank {
     private Customer[] allcustomer;
     private Branches[] allcbranches;
+    private int Salary;
     private int totalEmployee;
+
+    public int getSalary() {
+        return Salary;
+    }
+
+    public void setSalary(int salary) {
+        Salary = salary;
+    }
 
     public Customer[] getAllcustomer(Customer[] customerarray) {
         return allcustomer;
@@ -26,4 +35,36 @@ public class Bank {
     public void setTotalEmployee(int totalEmployee) {
         this.totalEmployee = totalEmployee;
     }
+    public void increaseSalary(int amount){
+        if(Salary<=amount) {
+            Salary = Salary + 1000;
+
+        }else{
+            Salary=Salary+0;
+
+
+
+        }
+       /* public String customerLookup(){    //customerLookuop(string fname). Looks for the customer and return customer data
+            int j;
+            for(int j=0;j<allcustomer.length;j++);
+            allcustomer[j].getFname();
+            return String;
+
+        */
+
+
+
+    }public void employeeWork(String name){          //find branchname where a employee works(lookup with fname)
+
+
+        int n;
+        for(n=0; n<allcbranches.length;n++){
+            allcbranches[n].getEmployeelist();
+            if(allcbranches.equals(name));
+            return;
+
+        }
+    }
+
 }
