c6865669791ea8641a8de8ecabbc04f6192785c2
rabinkhadka2000 <66097376+rabinkhadka2000@users.noreply.github.com>
2020-07-08 17:41:47 -0400

    Add files via upload

--- Test.java
+++ Test.java
@@ -1,15 +1,30 @@
-import java.util.Arrays;
+package FirstHeadJava;
 
 public class Test {
+    public static void main(String[] args) {
+       String[] Names={"Rabin","Roshan","Pabitra"};
+       for(int i=0;i<Names.length;i++) {
+           System.out.println(Names[i].toString().startsWith("R"));
+           Test xxx=new Test();
+           xxx.newG("R");
 
-	public static void main(String[] args) {
-		
-		Library newlib = new Library();
-		
-		
-		//newlib.getAllBooks("Roshan");
-		
-		
-	}
 
-}
+           }
+       }
+       public void newG(String anme ){
+           String[] Names={"Rabin","Roshan","Pabitra"};
+           for(int i=0;i<Names.length;i++) {
+               if(Names[i].startsWith("R"));
+               System.out.println(Names[i].startsWith("R"));
+
+           }
+
+
+
+        }
+
+    }
+
+
+
+
